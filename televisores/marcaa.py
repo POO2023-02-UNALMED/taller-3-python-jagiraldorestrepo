59f0ddd19f5f33f17832b07489936864f9fb96fa
jagiraldorestrepo <ja.giraldorestrepo@gmail.com>
2023-08-29 02:33:35 -0500

    --

---
+++ televisores/marcaa.py
@@ -0,0 +1,11 @@
+class Marca:
+
+    def __init__(self, nombre: str) :
+        self.nombre = nombre
+
+    def getNombre(self):
+        return self.nombre
+    
+    def setNombre(self, nombre:str):
+        self.nombre = nombre
+        
